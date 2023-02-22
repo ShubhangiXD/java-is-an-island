70f7a36113347e1f386f88f137873d0713610dad
ShubhangiXD <d.shubhangi1510@gmail.com>
2023-02-22 10:55:34 +0530

    new file:   ScannerRead.java

---
+++ ScannerRead.java
@@ -0,0 +1,11 @@
+//writing a program to display the user's name using Scanner class. 
+import java.util.*;
+public class ScannerRead {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Your name is: " + name);
+        sc.close();
+    }
+}
