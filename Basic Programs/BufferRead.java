2ad35323d5ca0ef619625fca4007a02193373b83
ShubhangiXD <d.shubhangi1510@gmail.com>
2023-02-22 10:59:35 +0530

    new file:   Basic Programs/BufferRead.java 	renamed:    ScannerRead.java -> Basic Programs/ScannerRead.java 	new file:   Basic Programs/Student.java

---
+++ Basic Programs/BufferRead.java
@@ -0,0 +1,9 @@
+import java.io.*;
+public class BufferRead {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter your name: ");
+        String name=br.readLine();
+        System.out.println("Your name is: " + name);
+    }
+}
