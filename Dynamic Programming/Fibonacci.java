0b2d304d8edd55c86841b8511434aebe9af87cc7
ShubhangiXD <d.shubhangi1510@gmail.com>
2023-02-28 09:25:37 +0530

    modified:   Dynamic Programming/Fibonacci.java

--- Dynamic Programming/Fibonacci.java
+++ Dynamic Programming/Fibonacci.java
@@ -1,7 +1,8 @@
+//program to find fibonacci pattern consisting n elements using dynamic programming
 import java.util.*;
 
 public class Fibonacci {
-    static int fibNo(int n) {
+    static void fibNo(int n) {
         int dp[] = new int[n + 2];
 
         dp[0] = 0;
@@ -9,17 +10,16 @@ public class Fibonacci {
         int i;
         for (i = 2; i <= n; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
+            System.out.print(dp[i-2]+ " ");
         }
-
-        return dp[n];
+        System.out.println(dp[n-1]);
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter n: ");
         int n = sc.nextInt();
-        int res = fibNo(n);
-        System.out.println(res);
+        fibNo(n);
         sc.close();
     }
 }
